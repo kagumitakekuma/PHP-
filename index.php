2f11dc2ce5405757af6e2088b453b376c02bd4c2
Kagumi Takekuma <80142118+kagumitakekuma@users.noreply.github.com>
2021-08-19 01:06:31 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -9,6 +9,16 @@ echo "It's \"Sunday\". Hello $name" . PHP_EOL;
 echo "It's \"Sunday\".\t Hello $name" . PHP_EOL;
 
 
+// $text = <<<'EOT' // nowdoc
+// $text = <<<"EOT" // heredoc
+  $text = <<<EOT
+  hello! $name
+    this is looooong
+  text!
+  
+  EOT;
+
+echo $text;
 
 
 
