1760d7285cd2235eb95b447cadaf2fcf794459fd
Kagumi Takekuma <80142118+kagumitakekuma@users.noreply.github.com>
2021-08-19 01:01:27 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+$name = 'me';
+
+//echo 'It's Sunday. Hello ' . $name . PHP_EOL;
+echo "It's Sunday. Hello " . $name . PHP_EOL;
+echo "It's Sunday. Hello $name" . PHP_EOL;
+echo "It's \"Sunday\". Hello $name" . PHP_EOL;
+echo "It's \"Sunday\".\t Hello $name" . PHP_EOL;
+
+
+
+
+
+
+?>
